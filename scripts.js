db08baabffd58f8dbf8123de32d0a4ff866bd859
Fredbaltimore <fredinmd410@gmail.com>
2020-08-24 21:29:35 -0400

    Cleaning up codebase

--- scripts.js
+++ scripts.js
@@ -1,53 +1,32 @@
-
-function randomJokes(){
-    var xmlhttp = new XMLHttpRequest();
-    var url = "https://api.chucknorris.io/jokes/random";
-    xmlhttp.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            var json = JSON.parse(this.responseText);
-            parseResponse(json);
-        }
-
+function randomJokes() {
+  const xmlhttp = new XMLHttpRequest();
+  const url = "https://api.chucknorris.io/jokes/random";
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const json = JSON.parse(this.responseText);
+      parseResponse(json);
     }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
 }
-function parseResponse(json){
-    document.getElementById("jokes").innerHTML = json["value"];
+function parseResponse(json) {
+  document.getElementById("jokes").innerHTML = json["value"];
 }
-document.getElementById("getjoke-btn").addEventListener("click", function(){
-    randomJokes();
+document.getElementById("getjoke-btn").addEventListener("click", function () {
+  randomJokes();
 });
 
-document.getElementById("p1").innerHTML = ("Generate your Chuck Norris Jokes Below");
+document.getElementById("p1").innerHTML =
+  "Generate your Chuck Norris Jokes Below";
 
-
-function imageSlides(){
-    
+function imageSlides() {
   let imageArray = ["cn2.jpg", "cn3.jpg", "cn4.jpg", "cn5.jpg"];
 
- imageArray.forEach((item, index) => {setTimeout(()=> 
-    {document.getElementById("slide").src = item
-     console.log(item, index)
-    }, 2000 * (index+1))}) 
+  imageArray.forEach((item, index) => {
+    setTimeout(() => {
+      document.getElementById("slide").src = item;
+      console.log(item, index);
+    }, 2000 * (index + 1));
+  });
 }
-
-
-// let greeting = prompt("Are you a fan of Chuck Norris?");
-// if(greeting === "yes"){
-//     alert("Great!, welcome to the site");
-
-// }
-// else{
-//     alert("Boo!");
-// }
-
-
-
-
-
-
-    
-    
-
